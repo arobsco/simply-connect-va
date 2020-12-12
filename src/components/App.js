b250f051b06244ede6096910f70549b8dc05b890
Andrew Roberts <github@codescotland.dev>
2020-12-12 14:54:13 +0000

    navigation framework

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Header from './Header';
 import { Col, Container, Row } from 'react-bootstrap';
-
+import Header from './Header';
+import NavigationBar from './NavigationBar';
 const App = () => {
     return(
         <Container fluid>
             <Row>
                 <Col md={12}>
                     <Header/>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12}>
+                    <NavigationBar/>
                 </Col>
             </Row>
         </Container>
