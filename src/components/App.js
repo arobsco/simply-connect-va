165af9f97bb501580ec06c7105a1089bdd1dfaaa
Andrew Roberts <github@codescotland.dev>
2020-12-12 14:38:50 +0000

    create header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-import { render } from "@testing-library/react";
-import React from "react";
+import React from 'react';
+import Header from './Header';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const App = () => {
     return(
-        <div>Hello world!</div>
+        <Container fluid>
+            <Row>
+                <Col md={12}>
+                    <Header/>
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
+
+const Header = () => {
+    return(
+        <Jumbotron fluid>
+            <Container>
+            <h1>Welcome to Simply Connect Virtual Airline!</h1>
+            </Container>
+        </Jumbotron>
+    )
+}
+
+export default Header;
