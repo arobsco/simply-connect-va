d58af48e9440a79152a8bd560f99449a29cdc43b
Andrew Roberts <github@codescotland.dev>
2020-12-13 17:32:03 +0000

    Define Menu Structure

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
 const NavigationBar = () => {
     return (
@@ -8,8 +8,27 @@ const NavigationBar = () => {
             <Navbar.Toggle aria-controls='basic-navbar-nav'/>
             <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav className='mr-auto'>
-                <Nav.Link as={Link} to='/'>Home</Nav.Link>
                 <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                <Nav.Link as={NavLink} to="/about">Join Us</Nav.Link>
+                <NavDropdown title='Flight Operations' id='basic-nav-dropdown'>
+                    <NavDropdown.Item>Todays Operations</NavDropdown.Item>
+                    <NavDropdown.Item>Live Map</NavDropdown.Item>
+                    <NavDropdown.Item>Our Fleet</NavDropdown.Item>
+                    <NavDropdown.Item>Our Hubs</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Crew Bag" id="basic-nav-dropdown">
+                    <NavDropdown.Item>Crew Center</NavDropdown.Item>
+                    <NavDropdown.Item>Setting Up FSAirlines</NavDropdown.Item>
+                    <NavDropdown.Item>Booking Flights</NavDropdown.Item>
+                    <NavDropdown.Item>Flying</NavDropdown.Item>
+                    <NavDropdown.Item>Aircraft Maintenance</NavDropdown.Item>
+                    <NavDropdown.Item>Ranks</NavDropdown.Item>
+                    <NavDropdown.Item>Rules</NavDropdown.Item>
+                    <NavDropdown.Item>Penalties</NavDropdown.Item>
+                    <NavDropdown.Item>Checklists and Guides</NavDropdown.Item>
+                    <NavDropdown.Item>Liveries</NavDropdown.Item>
+
+                </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
