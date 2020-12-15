319f306bd212cdaf12b954e3337ab46db0af70cd
Andrew Roberts <github@codescotland.dev>
2020-12-15 16:22:59 +0000

    fix insecure iframe

--- src/Containers/Operations/Fleet.js
+++ src/Containers/Operations/Fleet.js
@@ -5,7 +5,7 @@ const Fleet = () => {
     return (
         <div>
             <ResponsiveEmbed aspectRatio="16by9">
-                <embed src="http://remote.fsairlines.net/v1/fleet.php?rvi=48370&title=off" />
+                <embed src="https://remote.fsairlines.net/v1/fleet.php?rvi=48370&title=off" />
                 </ResponsiveEmbed>
                 </div>
     )
