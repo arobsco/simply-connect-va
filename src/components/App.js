4b482fffe7af257ed086f4d6cb54659f4c3150fa
Andrew Roberts <github@codescotland.dev>
2020-12-15 15:55:55 +0000

    Updates to navigation and carousel

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './Header';
 import NavigationBar from './NavigationBar';
 import Home from '../Containers/Home';
 import About from '../Containers/About';
@@ -13,16 +12,10 @@ const App = () => {
             <Row>
                 <Col md={12}>
                     <NavigationBar/>
-                    
                 </Col>
             </Row>
             <Row>
-                <Col md={12}>
-                    <Header/>
-                </Col>
-            </Row>
-            <Row>
-                <Col md={12}>
+                <Col md={12} className='mt-1'>
                     <Route path='/' exact component={Home}/>
                     <Route path='/about' component={About}/>
                 </Col>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -8,26 +8,26 @@ const NavigationBar = () => {
             <Navbar.Toggle aria-controls='basic-navbar-nav'/>
             <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav>
-                <Nav.Link as={NavLink} to="/about">About</Nav.Link>
-                <Nav.Link as={NavLink} to="/about">Join Us</Nav.Link>
+                <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
+                <Nav.Link as={NavLink} to='/join-us'>Join Us</Nav.Link>
                 <NavDropdown title='Flight Operations' id='basic-nav-dropdown'>
-                    <NavDropdown.Item>Todays Operations</NavDropdown.Item>
-                    <NavDropdown.Item>Live Map</NavDropdown.Item>
-                    <NavDropdown.Item>Our Fleet</NavDropdown.Item>
-                    <NavDropdown.Item>Our Hubs</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/operations/today'>Todays Operations</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/operations/map'>Live Map</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/operations/fleet'>Our Fleet</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/operations/hubs'>Our Hubs</NavDropdown.Item>
                 </NavDropdown>
-                <NavDropdown title="Crew Bag" id="basic-nav-dropdown">
-                    <NavDropdown.Item>Crew Center</NavDropdown.Item>
-                    <NavDropdown.Item>Setting Up FSAirlines</NavDropdown.Item>
-                    <NavDropdown.Item>Booking Flights</NavDropdown.Item>
-                    <NavDropdown.Item>Flying</NavDropdown.Item>
-                    <NavDropdown.Item>Aircraft Maintenance</NavDropdown.Item>
-                    <NavDropdown.Item>Ranks</NavDropdown.Item>
-                    <NavDropdown.Item>Rules</NavDropdown.Item>
-                    <NavDropdown.Item>Penalties</NavDropdown.Item>
-                    <NavDropdown.Item>Checklists and Guides</NavDropdown.Item>
-                    <NavDropdown.Item>Liveries</NavDropdown.Item>
-
+                <NavDropdown title='Crew Bag' id='basic-nav-dropdown'>
+                    <NavDropdown.Item href='https://www.fsairlines.net/crewcenter/' target='_blank' rel='noopener noreferrer'>Crew Center</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/setting-up'>Setting Up</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/training'>Training</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/booking'>Choosing and Booking Flights</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/flying'>Flying Tips</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/maintenance'>Aircraft Maintenance</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/ranks'>Ranks</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/rules'>Rules</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/checklists'>Checklists and Guides</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/liveries'>Liveries</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to='/crew/useful-links'>Useful Links</NavDropdown.Item>
                 </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
