1fb73b5be944d5885899ce430ca7d4866315f2ba
Andrew Roberts <github@codescotland.dev>
2020-12-15 22:20:48 +0000

    Fix import link

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import RotatingPlanes from '../components/rotatingPlanes/';
+import ImageCarousel from '../../Parts/ImageCarousel';
 const Home = () => {
     return(
         <div>
-            <RotatingPlanes />
+            <ImageCarousel />
             <div className='m-4'>
                 <p>Simply Connect is a virtual airline that needs a better copywriter than the one writing this text right now!</p>
             </div>
